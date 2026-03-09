6bea1b79a0ca979cca76c46e6ba657ef928a87a4
REDHAWKK <Princefrosh2007@gmail.com>
2026-03-09 08:36:46 -0700

    more changes

--- main.js
+++ main.js
@@ -113,3 +113,11 @@
                 }
             });
         });
+    /* ===============================
+       FOOTER YEAR
+    =============================== */
+    const year = document.getElementById("year");
+    if (year) {
+        year.textContent = new Date().getFullYear();
+    }
+    
